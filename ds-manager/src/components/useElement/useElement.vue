b0e6449c01bf3735d20501b1f9b0e39f52c50f2a
zlwk <1319942959@qq.com>
2019-06-20 22:03:08 +0800

    element-ui准备工作完成

--- ds-manager/src/components/useElement/useElement.vue
+++ ds-manager/src/components/useElement/useElement.vue
@@ -1,12 +1,10 @@
 <template>
-  <div id="app">
-    <router-view/>
-  </div>
+    <div>我是useElement.vue</div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+
 }
 </script>
 
--- ds-manager/src/router/index.js
+++ ds-manager/src/router/index.js
@@ -1,10 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-
+import UseElement from '../components/useElement/useElement'
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    
+    {path:'/useElement',name:'useElement',component:UseElement}
   ]
 })
