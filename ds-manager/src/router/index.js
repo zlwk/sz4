2aa25f3fd5682025b1713324fe93a103c816c3e8
zlwk <1319942959@qq.com>
2019-06-21 21:08:04 +0800

    建立home路由 组件

--- ds-manager/src/router/index.js
+++ ds-manager/src/router/index.js
@@ -2,11 +2,14 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import UseElement from '../components/useElement/useElement'
 import Login from '../components/login/login'
+import Home from '../components/home/home'
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {path:'/useElement',name:'useElement',component:UseElement},
-    {path:'/login',name:'login',component:Login}
+    {path:'/login',name:'login',component:Login},
+    {path:'/home',name:'home',component:Home},
+    {path:'/',redirect:'/home'}
   ]
 })
